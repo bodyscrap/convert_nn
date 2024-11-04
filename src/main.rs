303a28040657289b42cf95f26ce82764bea5f5f3
bodyscrap <svkcyberbots@gmail.com>
2024-11-05 00:26:58 +0900

    モデルの読み込みまで実装

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use anyhow::Result;
+use candle_core::{Device, DType};
 use convert_nn::CNN;
+use candle_nn::var_builder::VarBuilder;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    // デバイスの設定
+    let device = Device::cuda_if_available(0)?; // GPUを使用
+    let model_path = std::path::PathBuf::from("mnist_cnn_weights.safetensors");
+    let mut vb = unsafe 
+    { 
+        VarBuilder::from_mmaped_safetensors(&[model_path], DType::F32, &device)? 
+    };
+    // CNNモデルの作成
+    let mut model = CNN::new(&mut vb)?;
+    // ここで、modelを使用して推論や評価を実行
+    Ok(())
 }
